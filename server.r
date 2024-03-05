b489690660268084c3b68abea8f619fdeb452840
CaioMussatto <caio.mussatto@unesp.br>
2024-03-05 13:48:01 -0300

    Gene set update 2

--- server.r
+++ server.r
@@ -1,6 +1,5 @@
-server = function(input, output, session) {
-  
-  selected_df <- reactive ({
+server <- function(input, output, session) {
+  selected_df <- reactive({
     req(input$human, input$cell)
     data <- lineplot[[input$human]][[input$cell]]
     if (input$gene_set != "All") {
@@ -10,54 +9,59 @@ server = function(input, output, session) {
       data <- data
     }
   })
-  
+
   morpheus_result <- reactive({
     req(input$human, input$cell)
     morpheus_result <- Morpheus(selected_df())
     return(morpheus_result)
   })
-  
-  
+
+
   pathways_result <- reactive({
     req(input$enrichr, input$enrichr2)
     pathways_result <- pathways(vias[[input$enrichr]], dbs[[input$enrichr2]])
   })
-  
+
   genes_used <- reactive({
     req(input$enrichr)
     genes_used <- dataframe(vias[[input$enrichr]])
   })
-  
+
   output$heatmap <- renderPlot({
     morpheus_data <- morpheus_result()
     title <- paste("Human primary", input$human, "cancer vs", str_to_title(input$cell), "cell lines")
-    nomes <- HeatmapAnnotation(df = as.data.frame(morpheus_data["Subtype"]), which = "row", annotation_width = unit(c(1, 3), 'cm'),
-                               gap = unit(1, 'mm'), annotation_name_side = "top", annotation_name_rot = 45)
-    morpheus_data <- as.matrix(morpheus_data[,2:length(morpheus_data)])
-    Heatmap(morpheus_data, right_annotation = nomes, rect_gp = gpar(col = "white", lwd = 0.8),
-            column_title = title, column_title_gp = gpar(fontsize = 14, fontface = "bold"),
-            column_names_side = "top", column_names_gp = gpar(fontsize = 12), column_names_rot = 80,
-            row_names_gp = gpar(fontsize = 10),cell_fun = function(j, i, x, y, width, height, fill) {
-              grid.text(sprintf("%.3f", morpheus_data[i, j]), x, y, gp = gpar(fontsize = 10))},
-            heatmap_legend_param = list(title = "R value", at = seq(-1, 1, 0.5)), width = 500)
+    nomes <- HeatmapAnnotation(
+      df = as.data.frame(morpheus_data["Subtype"]), which = "row", annotation_width = unit(c(1, 3), "cm"),
+      gap = unit(1, "mm"), annotation_name_side = "top", annotation_name_rot = 45
+    )
+    morpheus_data <- as.matrix(morpheus_data[, 2:length(morpheus_data)])
+    Heatmap(morpheus_data,
+      right_annotation = nomes, rect_gp = gpar(col = "white", lwd = 0.8),
+      column_title = title, column_title_gp = gpar(fontsize = 14, fontface = "bold"),
+      column_names_side = "top", column_names_gp = gpar(fontsize = 12), column_names_rot = 80,
+      row_names_gp = gpar(fontsize = 10), cell_fun = function(j, i, x, y, width, height, fill) {
+        grid.text(sprintf("%.3f", morpheus_data[i, j]), x, y, gp = gpar(fontsize = 10))
+      },
+      heatmap_legend_param = list(title = "R value", at = seq(-1, 1, 0.5)), width = 500
+    )
   })
-  
-  tabela_result_filtered <- reactiveVal(NULL)  
-  
+
+  tabela_result_filtered <- reactiveVal(NULL)
+
   tabela_result <- reactive({
     req(input$human, input$cell)
     tabela_result <- cor_p_value(selected_df())
   })
-  
-  observeEvent(input$filter_button, { 
-    tabela_filtered <- tabela_result() %>% 
+
+  observeEvent(input$filter_button, {
+    tabela_filtered <- tabela_result() %>%
       filter(str_detect(Human_Cancer, paste0("^", input$filter, collapse = "|")))
-    tabela_result_filtered(tabela_filtered)  
+    tabela_result_filtered(tabela_filtered)
   })
 
   observeEvent(input$gene_set, {
     tabela_result_filtered(NULL)
-  }) 
+  })
 
   output$tabela <- DT::renderDT({
     if (!is.null(tabela_result_filtered())) {
@@ -76,23 +80,29 @@ server = function(input, output, session) {
       } else {
         write.csv(tabela_result, file, row.names = FALSE)
       }
-    })
-  
-  output$enrichr_plot <- renderPlot({
-    grafico <- pathways_result()
-    ggplot(grafico[1:10,], aes(x=`Combined.Score`, y=`Term`, label = round(`Combined.Score`))) +
-      geom_segment(aes(x = 0, y = `Term`, xend = `Combined.Score`, yend = `Term`), linetype = "longdash") +
-      geom_point(size = 8, color = "#f2545b") + theme(text = element_text(size = 14)) + 
-      geom_text(color = 'black', size = 3, position = "identity", fontface = "bold.italic") +
-      theme_bw() +
-      xlab("") + ylab("")
-  }, res = 100)
-  
+    }
+  )
+
+  output$enrichr_plot <- renderPlot(
+    {
+      grafico <- pathways_result()
+      ggplot(grafico[1:10, ], aes(x = `Combined.Score`, y = `Term`, label = round(`Combined.Score`))) +
+        geom_segment(aes(x = 0, y = `Term`, xend = `Combined.Score`, yend = `Term`), linetype = "longdash") +
+        geom_point(size = 8, color = "#f2545b") +
+        theme(text = element_text(size = 14)) +
+        geom_text(color = "black", size = 3, position = "identity", fontface = "bold.italic") +
+        theme_bw() +
+        xlab("") +
+        ylab("")
+    },
+    res = 100
+  )
+
   output$genes <- DT::renderDT({
     DT::datatable(genes_used(), rownames = F)
   })
-  
+
   output$tabela_enrichr <- DT::renderDT({
-    DT::datatable(pathways_result()[,c(1,4,8)], rownames = F, options = list(pageLength = 5))
+    DT::datatable(pathways_result()[, c(1, 4, 8)], rownames = F, options = list(pageLength = 5))
   })
 }
